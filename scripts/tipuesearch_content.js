82a1ee53c4755fd4ae87f33eb23772d50c7820ce
Bryce Hoeper <Brycehoeper@gmail.com>
2014-10-26 16:02:03 -0700

    whole bunch of changes
    woot

---
+++ scripts/tipuesearch_content.js
@@ -0,0 +1,13 @@
+var tipuesearch = {"pages": [
+     {"title": "Image Block Help Guide", "text": "The help.Squarespace.com Image Block guide", "tags": "image", "loc": "http://help.squarespace.com/guides/about-the-image-block"},
+     {"title": "Gallery Block Help Guide", "text": "The help.Squarespace.com Gallery Block guide", "tags": "Gallery", "loc": "http://help.squarespace.com/guides/about-the-gallery-block"},
+     {"title": "", "text": "", "tags": "", "loc": ""},
+     {"title": "", "text": "", "tags": "", "loc": ""},
+     {"title": "", "text": "", "tags": "", "loc": ""},
+     {"title": "", "text": "", "tags": "", "loc": ""},
+     {"title": "", "text": "", "tags": "", "loc": ""},
+     {"title": "", "text": "", "tags": "", "loc": ""},
+     {"title": "", "text": "", "tags": "", "loc": ""},
+]};
+
+
